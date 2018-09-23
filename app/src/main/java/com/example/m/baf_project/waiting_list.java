1542123411476ea933d38f1d4695d87c0b44cdb9
Redhoy <redhoy007@gmail.com>
2018-09-23 23:55:52 +0600

    ashraf1

---
+++ app/src/main/java/com/example/m/baf_project/waiting_list.java
@@ -0,0 +1,13 @@
+package com.example.m.baf_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class waiting_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waitinglist);
+    }
+}
